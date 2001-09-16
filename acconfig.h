e87271c7e36d9f084250fe17fafc50ac3b58fc5d
hiroo <hiroo@62ffc7d7-c204-4b8a-a13a-9c32274df18a>
2001-09-16 11:35:57 +0000

    Fixed some comment descriptions. --enable-ipv6, --enable-unsafe-path, --enable-debug options, etc.
    git-svn-id: file:///home/mitsutoshi/src/sourceforge.jp/rsync/svn/trunk@51 62ffc7d7-c204-4b8a-a13a-9c32274df18a

--- acconfig.h
+++ acconfig.h
@@ -1,10 +1,16 @@
-/* $Id: acconfig.h,v 1.1 2001-06-18 09:09:20 ura Exp $ */
+/* $Id: acconfig.h,v 1.2 2001-09-16 11:35:57 hiroo Exp $ */
 
-/* Define to `long' if <sys/types.h> doesn't define.  */
+/* Define to `long' if <sys/types.h> does not define.  */
 #undef time_t
 
-/* Define to 'socklen_t' if <sys/socket.h> does'n define. */
+/* Define to 'socklen_t' if <sys/socket.h> does not define. */
 #undef socklen_t
 
-/* Define to 'RAND_MAX' if <stdlib.h> does'n define. */
+/* Define to 'RAND_MAX' if <stdlib.h> does not define. */
 #undef HAVE_RAND_MAX
+
+/*
+ * Defined if you allow creating a file in an arbitrary path
+ * which is the traditional feature.
+ */
+#undef WNN_ALLOW_UNSAFE_PATH
