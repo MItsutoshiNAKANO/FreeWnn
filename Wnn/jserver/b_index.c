d29d68c7051b87dcd38fdfa0e7f2dc2c70ae6d84
hiroo <hiroo@62ffc7d7-c204-4b8a-a13a-9c32274df18a>
2002-03-08 18:07:35 +0000

    include appropriate system header.
    git-svn-id: file:///home/mitsutoshi/src/sourceforge.jp/rsync/svn/trunk@75 62ffc7d7-c204-4b8a-a13a-9c32274df18a

--- Wnn/jserver/b_index.c
+++ Wnn/jserver/b_index.c
@@ -1,5 +1,5 @@
 /*
- *  $Id: b_index.c,v 1.3 2001-06-14 18:15:59 ura Exp $
+ *  $Id: b_index.c,v 1.4 2002-03-08 18:07:35 hiroo Exp $
  */
 
 /*
@@ -10,7 +10,7 @@
  *                 1987, 1988, 1989, 1990, 1991, 1992
  * Copyright OMRON Corporation. 1987, 1988, 1989, 1990, 1991, 1992, 1999
  * Copyright ASTEC, Inc. 1987, 1988, 1989, 1990, 1991, 1992
- * Copyright FreeWnn Project 1999, 2000
+ * Copyright FreeWnn Project 1999, 2000, 2002
  *
  * Maintainer:  FreeWnn Project   <freewnn@tomo.gr.jp>
  *
@@ -32,6 +32,7 @@
 /**  cWnn  Version 1.1   **/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "commonhd.h"
 #include "de_header.h"
 #include "jdata.h"
