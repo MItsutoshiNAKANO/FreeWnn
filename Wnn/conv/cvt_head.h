8bca97b7b0000c2b173ae343888266ba9275ced4
hiroo <hiroo@62ffc7d7-c204-4b8a-a13a-9c32274df18a>
2002-03-21 03:32:45 +0000

    cvt_head.h: remove commented out lines. cvt_read.c: string.h/strings.h issue. Do not see if BSD42 is defined anymore.
    git-svn-id: file:///home/mitsutoshi/src/sourceforge.jp/rsync/svn/trunk@83 62ffc7d7-c204-4b8a-a13a-9c32274df18a

--- Wnn/conv/cvt_head.h
+++ Wnn/conv/cvt_head.h
@@ -1,5 +1,5 @@
 /*
- *  $Id: cvt_head.h,v 1.4 2001-06-14 18:15:54 ura Exp $
+ *  $Id: cvt_head.h,v 1.5 2002-03-21 03:32:45 hiroo Exp $
  */
 
 /*
@@ -10,7 +10,7 @@
  *                 1987, 1988, 1989, 1990, 1991, 1992
  * Copyright OMRON Corporation. 1987, 1988, 1989, 1990, 1991, 1992, 1999
  * Copyright ASTEC, Inc. 1987, 1988, 1989, 1990, 1991, 1992
- * Copyright FreeWnn Project 1999, 2000
+ * Copyright FreeWnn Project 1999, 2000, 2002
  *
  * Maintainer:  FreeWnn Project   <freewnn@tomo.gr.jp>
  *
@@ -36,27 +36,8 @@
         convert_key.c 及び convert_read.c で使われる
         へッダファイル。テーブルのサイズの定義など。
 ***********************************************************************/
-/*      Version 4.0
- */
-#ifdef  nodef
-#if defined(luna)
-#if defined(uniosu)
-# ifndef        SYSVR2
-#  define       SYSVR2
-# endif
-#else /* if defined(MACH) || defined(uniosb) */
-# ifndef        BSD42
-#  define       BSD42
-# endif
-#endif
-#else
-#if defined(sun)
-# ifndef        BSD42
-#  define       BSD42
-# endif
-#endif
-#endif
-#endif
+/*	Version 4.0	*/
+
 #include "commonhd.h"
 
 #if defined(luna) || defined(DGUX)
--- Wnn/conv/cvt_read.c
+++ Wnn/conv/cvt_read.c
@@ -1,5 +1,5 @@
 /*
- *  $Id: cvt_read.c,v 1.3 2001-06-14 18:15:54 ura Exp $
+ *  $Id: cvt_read.c,v 1.4 2002-03-21 03:32:45 hiroo Exp $
  */
 
 /*
@@ -10,7 +10,7 @@
  *                 1987, 1988, 1989, 1990, 1991, 1992
  * Copyright OMRON Corporation. 1987, 1988, 1989, 1990, 1991, 1992, 1999
  * Copyright ASTEC, Inc. 1987, 1988, 1989, 1990, 1991, 1992
- * Copyright FreeWnn Project 1999, 2000
+ * Copyright FreeWnn Project 1999, 2000, 2002
  *
  * Maintainer:  FreeWnn Project   <freewnn@tomo.gr.jp>
  *
@@ -41,14 +41,15 @@
  */
 #include <stdio.h>
 #include <ctype.h>
+#if STDC_HEADERS
+#  include <string.h>
+#elif HAVE_STRINGS_H
+#  include <strings.h>
+#endif /* STDC_HEADERS */
+
 #include "cvt_head.h"
 
-#ifdef BSD42
-#include <strings.h>
-#endif
-
 #ifdef SYSVR2
-#include <string.h>
 #include <fcntl.h>
 #ifdef TERMINFO
 #include <curses.h>
