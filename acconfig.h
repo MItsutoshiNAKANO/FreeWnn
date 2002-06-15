cd80289f0c71f6c68249d43bea2b39180c52d151
hiroo <hiroo@62ffc7d7-c204-4b8a-a13a-9c32274df18a>
2002-06-15 10:19:27 +0000

    acconfig.h:  * Added RETSIGTYPE_VOID hack taken from EB Library    <URI:http://www.sra.co.jp/people/m-kasahr/eb/>.  * Small cosmetic changes. configure.in:  * Removed -DCSRG_BASED for CCOPTIONS of *BSD.  * Aggregated AC_CHECK_HEADERS and AC_CHECK_FUNCS parameter to reduce the    size of configure a little.  * Cosmetic change in AC_CHECK_TYPES usage.  * Added RETSIGTYPE_VOID hack taken from EB Library.  * Suppress Makefile generation for each language when they are disabled. config.h.in, configure: regenerate. (GNU Autoconf 2.53)
    git-svn-id: file:///home/mitsutoshi/src/sourceforge.jp/rsync/svn/trunk@124 62ffc7d7-c204-4b8a-a13a-9c32274df18a

--- acconfig.h
+++ acconfig.h
@@ -1,12 +1,12 @@
-/* $Id: acconfig.h,v 1.2 2001-09-16 11:35:57 hiroo Exp $ */
+/* $Id: acconfig.h,v 1.3 2002-06-15 10:19:27 hiroo Exp $ */
+
+/* Define `socklen_t' to int if <sys/socket.h> does not define. */
+#undef socklen_t
 
 /* Define to `long' if <sys/types.h> does not define.  */
 #undef time_t
 
-/* Define to 'socklen_t' if <sys/socket.h> does not define. */
-#undef socklen_t
-
-/* Define to 'RAND_MAX' if <stdlib.h> does not define. */
+/* Define to `RAND_MAX' if <stdlib.h> does not define. */
 #undef HAVE_RAND_MAX
 
 /*
@@ -14,3 +14,6 @@
  * which is the traditional feature.
  */
 #undef WNN_ALLOW_UNSAFE_PATH
+
+/* Define if the signal function returns void. */
+#undef RETSIGTYPE_VOID
