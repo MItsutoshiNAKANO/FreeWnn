a919a30399ba743e13b3866c8be3166b21717afa
aonoto <aonoto@62ffc7d7-c204-4b8a-a13a-9c32274df18a>
2006-02-10 18:29:38 +0000

    - Include string related header to reduce warning. (Merged from   hiroo's work tree.) - Stripped old mail address (as usual).
    git-svn-id: file:///home/mitsutoshi/src/sourceforge.jp/rsync/svn/trunk@220 62ffc7d7-c204-4b8a-a13a-9c32274df18a

--- Wnn/uum/basic_op.c
+++ Wnn/uum/basic_op.c
@@ -1,5 +1,5 @@
 /*
- *  $Id: basic_op.c,v 1.4 2002-05-12 22:51:17 hiroo Exp $
+ *  $Id: basic_op.c,v 1.5 2006-02-10 18:29:38 aonoto Exp $
  */
 
 /*
@@ -10,9 +10,9 @@
  *                 1987, 1988, 1989, 1990, 1991, 1992
  * Copyright OMRON Corporation. 1987, 1988, 1989, 1990, 1991, 1992, 1999
  * Copyright ASTEC, Inc. 1987, 1988, 1989, 1990, 1991, 1992
- * Copyright FreeWnn Project 1999, 2000, 2002
+ * Copyright FreeWnn Project 1999, 2000, 2002, 2006
  *
- * Maintainer:  FreeWnn Project   <freewnn@tomo.gr.jp>
+ * Maintainer:  FreeWnn Project
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -43,9 +43,13 @@ bufferの使い方とかbufferの内容がどのように画面に表示されるかとか
 #include <stdio.h>
 #if STDC_HEADERS
 #  include <stdlib.h>
+#  include <string.h>
 #else
 #  if HAVE_MALLOC_H
 #    include <malloc.h>
+#  endif
+#  if HAVE_STRINGS_H
+#    include <strings.h>
 #  endif
 #endif /* STDC_HEADERS */
 #include "jllib.h"
