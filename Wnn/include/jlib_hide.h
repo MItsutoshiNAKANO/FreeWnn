08f38ed1aaaf8ca4787b897863ac8f3bac07ec78
itisango <itisango@62ffc7d7-c204-4b8a-a13a-9c32274df18a>
2014-08-01 22:04:26 +0000

    Added Copyright and Licenses messages:
     Modified Files:
    	GPL-2+:
     	Wnn/include/conv.h Wnn/include/etc.h
     	Wnn/include/jutil.h
    	LGPL-2+
    	Wnn/include/romkan.h Wnn/include/jlib_hide.h
    
    
    git-svn-id: file:///home/mitsutoshi/src/sourceforge.jp/rsync/svn/trunk@272 62ffc7d7-c204-4b8a-a13a-9c32274df18a

--- Wnn/include/jlib_hide.h
+++ Wnn/include/jlib_hide.h
@@ -1,3 +1,35 @@
+/*
+ * $Id: jlib_hide.h,v 1.2 2014-08-01 22:04:26 itisango Exp $
+ */
+/*
+ * FreeWnn is a network-extensible Kana-to-Kanji conversion system.
+ * This file is part of FreeWnn.
+ * 
+ * Copyright Kyoto University Research Institute for Mathematical Sciences
+ *                 1987, 1988, 1989, 1990, 1991, 1992
+ * Copyright OMRON Corporation. 1987, 1988, 1989, 1990, 1991, 1992, 1999
+ * Copyright ASTEC, Inc. 1987, 1988, 1989, 1990, 1991, 1992
+ * Copyright FreeWnn Project 1999, 2000, 2001, 2002, 2003, 2013
+ *
+ * Maintainer:  FreeWnn Project   <freewnn@tomo.gr.jp>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
 #ifndef FRWNN_JLIB_HIDE_H
 #define FRWNN_JLIB_HIDE_H 1
 
